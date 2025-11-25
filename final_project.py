9c4de39e4c7ad2a5dd20d00fe1dc900b184ab2ef
JV-546 <v.jathiesh@hotmail.com>
2025-11-25 16:17:26 -0500

    added colors and fixed vertical win check

--- final_project.py
+++ final_project.py
@@ -1,22 +1,28 @@
 #////////////////// FINAL PROJECT \\\\\\\\\\\\\\\\\\\\\\\\\\\
 import random
+from termcolor import colored,cprint
 size = 3
 def make_board(size):
-    #function the create board of any size (n x n)
+    # create board using list of size (n x n)
     return [[" " for _ in range(size)] for _ in range(size)]
 
 def print_board(board):
     #function to print the board
+    
     # print colums
     print("\n    ",end="")
+    #print colums numbers
     for i in range(size):
         print(i+1,end="   ")
     print()
+    
     # print the dviding lines
     line = "  " + "-" * (size * 4 + 1)
     print(line)
+    
     # print row
     for r in range(size):
+         #print row numbers
         print(f"{r+1} | ",end="")
         print(" | ".join(board[r]),end=" |\n")
         print(line)
@@ -25,32 +31,36 @@ def print_board(board):
 
 def player_move(board, symbol):
     while True:
-        # get player's position choice   
-        move = input(f"Player {symbol}, enter a two numbers (x,y) from 1 to {size} : ").replace(" ","")
+        # get player's position choice
+        cprint(f"\t////////// PLAYER",end="",attrs=["bold"])
+        cprint(f" {symbol}","blue",attrs=["bold"],end="")
+        cprint(" //////////",attrs=["bold"])   
+        move = input(f"Enter a two numbers (x,y) from 1 to {size} :\n").replace(" ","")
         try:    
             x_str,y_str =  (move.split(","))
         except:
-            print("Provide a valid format! Two numbers seperated by a comma: x,y ")
+            cprint("Provide a valid format! Two numbers seperated by a comma: x,y ","yellow")
             continue
+        #convert string to int
         x = int(x_str)
         y = int(y_str)
-        # make sure its within the right range 1 to the size of the board
+        # make sure it's within the right range (1-size) of the board
         if x < 1 or x > size or  y < 1 or y > size:
-            print("Please provide two numbers between 1 and {size}.")
+            cprint("Please provide two numbers between 1 and {size}.\n","yellow")
             continue
         
         # convert to zero index
-        x -=1
-        y -=1
+        x -= 1
+        y -= 1
 
         # check if the spot is free
         if board[x][y] != " ":
-            print("That spot is taken!")
+            cprint(f"The spot ({[x+1]},{[y+1]})  is taken!","red")
             continue
 
         # else place player symbol
         else:
-            board[x][y] = symbol
+            board[x][y] = colored(symbol,"blue")
             break
 
 
@@ -64,20 +74,24 @@ def empty_space(board):
     return empty
 
 def computer_move(board, symbol):
-    print("computer places ...")
+    cprint("\t////////// Computer Thinking //////////","green",attrs=["bold"])
     x,y = random.choice(empty_space(board))
-    board[x][y] = symbol
+    cprint(f"\t\tComputer Plays ... ({x},{y})","green")
+    board[x][y] = colored(symbol, "light_green")
 
     
 def check_win(board):
-    #check horizontal lines
+
+ #///// check horizontal win conditions \\\\\   
+   
+    #check top rows if empty
     for x in range(size):
         first = board[x][0]
         if first == " ":
             continue
 
         match = True
-        # check all colums for this row
+        # check all cells  in this row to match first's symbol
         for y in range(size):
             if board[x][y] != first:
                 match = False
@@ -85,20 +99,22 @@ def check_win(board):
         if match:
             return True
         
-     #check vertical lines
-        for y in range(size):
-            first = board[0][y]
-            if first == " ":
-                continue
+ #///// check Vertical  win conditions \\\\\           
+     
+     #check top colums if empty
+    for y in range(size):
+        first = board[0][y]
+        if first == " ":
+            continue
 
-            match = True
-            # check all colums for this row
-            for x in range(size):
-                if board[x][y] != first:
-                    match = False
-                    break
-            if match:
-                return True
+        match = True
+        # check all cells in this colum to match the first's symbol
+        for x in range(size):
+            if board[x][y] != first:
+                match = False
+                break
+        if match:
+            return True
     # if no one has won yet
     return False
 
@@ -110,36 +126,46 @@ def game(mode):
     board = make_board(size)
     #ask player for the symbole they wish to play as (X or O)
     while True:
-        player = input("Choose you symbol X or O: ").upper() 
-        if player in ("X","O"):
+        player = input("Choose you symbol X or O: ").upper()
+        if player in ("X","O"):           
             break
 
     #assign the opposite symbol to the computer     
     if player == "X":
-        computer = "O"
+        computer = colored("O","green")
     elif player == "O":
-        computer = "X" 
+        computer = colored("X","green") 
     # start the game with the player's turn
     turn = player
 
     while True:
         print_board(board)
 
+        # player always makes the first move
         if turn == player:
             player_move(board,player)
         else:
+            # check mode to determine if  player 2 or computer turn
             if mode == 1:
                  player_move(board, computer)          
             else:
                computer_move(board, computer)
-                
+        # check win conditions        
         if check_win(board):
             print_board(board)
-            print(f"{turn} WINS! ")
+            if turn == "X":
+                cprint(f"\t////// Player ",attrs=["bold"],end="")
+                cprint(f"{turn} ","blue",attrs=["bold"],end="")
+                cprint("WINS! ////// ",attrs=["bold"])
+            elif turn == "O":
+                cprint(f"\t////// Player ",attrs=["bold"],end="")
+                cprint(f"{turn} ","green",attrs=["bold"],end="")
+                cprint("WINS! ////// ",attrs=["bold"])
             return
+        # check for a Tie
         if not empty_space(board):
             print_board(board)
-            print("Its a Tie! ")
+            cprint("\t Its a Tie!","magenta",attrs=["bold"])
             return
         
         # change turns
@@ -148,17 +174,16 @@ def game(mode):
         elif turn == "O":
             turn = "X" 
         
-
-
 def main():
     while True:
         try:
+            cprint("\_____________ TIC TAC TOE _____________/\n","cyan", attrs=["bold"])
             mode = int(input("1) Player 1 v/s Player 2 \n2) Play against Computer  \nMODE: "))
             if mode not in(1,2):
-                print("Please provide a correct mode 1 or 2")
+                cprint("Please provide a correct mode 1 or 2 ","light_red")
                 continue
         except ValueError:
-            print("Please provide a correct mode 1 or 2")
+            cprint("Please provide a correct mode 1 or 2 ","light_red")
             continue
         game(mode)
         while True:
